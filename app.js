644daadcfa0877a7e977f16489160ab69e0bf6e0
Tobi Ajibade <herityjohnny14>
2020-11-30 14:59:48 -0800

    Schema and resolvers created

--- app.js
+++ app.js
@@ -5,45 +5,117 @@ const { gql, ApolloServer } = require('apollo-server');
 // const app = express();
 
 const typeDefs = gql`
+    # Object types
     type Book {
         title: String
-        id: String
-        author: String
+        id: Int
+        author: Author
+    }
+
+    type Author {
+        name: String
+        books: [Book]
     }
 
     # A query is the graphs entry point.
-    # @returns {Array} books - Retutns an array of books.
+    # @returns {Array} books - Returns an array of books.
     type Query {
-        books:[Book]
+        "A book query"
+        books:[Book] 
+        """
+        A author type returns array
+        """
+        authors: [Author]
+        book(id: Int): Book
+
+        author(id: Int): Author
+    }
+
+    type Mutation {
+        addBook(data:AddBookInput) : Book
+    }
+
+    # A special type of object
+    input AddBookInput {
+        title: String
+        id: Int
     }
 `;
+
+
+// Interface - should be in a schema 
+// interface MutationResponse {
+//     code: String!
+//     success: Boolean!
+//     message: String!
+// }
+
+// " Implementation of the Mutation Response on the addBook type """
+// type AddBookMutationResponse {
+//     code: String!
+//     success: Boolean!
+//     message: String!
+//     book: Book
+// }
 
 // This could be the data from a database
 const iwe = [
     {
         title: "The Coder",
-        id: 1,
-        author: "Tobi Ajibade"
+        id: 1
     },
     {
-        title: "The algrithms",
-        id: 2,
-        author: "Shade Will"
+        title: "The algorithms",
+        id: 2
     }
 ];
+
+const authors = [
+    {
+        name: "Tobi Ajibade",
+        id: 1
+    },
+    {
+        name: "Shade Will",
+        id: 2
+    }
+]
 
 // Resolver - tells the apollo server how to fetch the data for a particular data types
 
 const resolvers = {
     Query: {
-        books: () => iwe
+        books: () => iwe,
+        authors: () => authors,
+        book: (_, args, context) => {
+            return iwe.find(bk => { return bk.id === args.id });
+        },
+        author: (_, args, context) => {
+            return authors.find(author => author.id === args.id);
+        }
+    },
+    Book: {
+        author: (parent) => {
+            return authors.find(author => author.id === parent.id);
+        }
+    },
+    Author: {
+        books: (parent) => {
+            return iwe.find(books => books.id === parent.id);
+        }
+    },
+    Mutation: {
+        addBook: (_, args, context) => {
+            return args.data;
+            // TODO check if book already exist
+        }
     }
 }
 
 
 //Initialize an instance of apollo server
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: ({ req, res }) => ({ req, res }) });
 
 
 
